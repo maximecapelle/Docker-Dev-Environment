8bd42bfdb8f26b8dd1db403afb9da1ea815c6d3b
Maxime Capelle <max.luc.cap@gmail.com>
2024-09-15 09:35:35 +0200

    implemented gz sim compatibility

--- scripts/entrypoints/default_entrypoint.sh
+++ scripts/entrypoints/default_entrypoint.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Function to source the correct ROS setup.bash
+# FUNCTIONS
 source_ros() {
     if [ -n "$ROS_DISTRO" ]; then
         ROS_SETUP="/opt/ros/$ROS_DISTRO/setup.bash"
         if [ -f "$ROS_SETUP" ]; then
-            echo "Sourcing ROS distro: $ROS_DISTRO from $ROS_SETUP"
+            # echo "Sourcing ROS distro: $ROS_DISTRO from $ROS_SETUP"
             source "$ROS_SETUP"
         else
             echo "Error: Could not find ROS setup file for $ROS_DISTRO at $ROS_SETUP"
         fi
-    else
-        echo "Error: ROS_DISTRO environment variable is not set."
     fi
 }
 
-# Source ROS
-source_ros  # Correct function name
+
+
+## PRINTING VERSIONS
+echo -e "\n\n"
 
 # Print OS version
 OS_VERSION=$(lsb_release -d | awk -F"\t" '{print $2}')
-echo -e "\n\n"
-echo "OS: $OS_VERSION"
+if [ -n "$OS_VERSION" ]; then
+    echo "OS: $OS_VERSION"
+fi
 
 # # Print ROS version
-echo "ROS$ROS_VERSION: $ROS_DISTRO"
-echo -e "\n\n"
+if [ -n "$ROS_DISTRO" ]; then
+    # Source ROS
+    source_ros  # Correct function name
+    echo "ROS$ROS_VERSION: $ROS_DISTRO"
+fi
 
+# # Print Gazebo Environment
+if [ -n "$GAZEBO_VERSION" ]; then
+    echo "Gazebo: $GAZEBO_VERSION"
+fi
+
+
+
+echo -e "\n\n"
 # Start an interactive shell
 exec "$@"
